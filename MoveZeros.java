d5b44a786addf36533899f6382ae15753c810fc7
vineesh91 <vineeshravindran@gmail.com>
2018-01-19 17:26:29 -0800

    new

---
+++ MoveZeros.java
@@ -0,0 +1,16 @@
+public class MoveZeros {
+    public void moveZeroes(int[] nums) {
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0) {
+                int k = i;
+                for(int j = i+1; j < nums.length; j++) {
+                    if(nums[j] != 0) {
+                        nums[k] = nums[j];
+                        nums[j] = 0;
+                        k = j;
+                    }
+                }
+            }
+        }
+    }
+}
