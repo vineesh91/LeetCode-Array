b95f278faabb0cf0268f9e6c68755e1c249fe4c4
vineesh <vineeshravindran@gmail.com>
2018-06-05 16:57:57 -0400

    Shorest Dis to char

--- ShortestDistanceToCharacter.java
+++ ShortestDistanceToCharacter.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ShortestDistanceToCharacter {
+    //Takes more time less space
     public int[] shortestToChar(String S, char C) {
         int N = S.length();
         int[] ret = new int[N];
@@ -34,5 +35,42 @@ public class ShortestDistanceToCharacter {
             }
         }
         return ret;
+    }
+    //Takes less time more space
+    public int[] shortestToChar2(String S, char C) {
+        int N = S.length();
+        int[] arr1 = new int[N];
+        int[] arr2 = new int[N];
+        int start = -1;
+        for(int i = 0; i < N; i++) {
+            if(S.charAt(i) == C) {
+                start = i;
+                arr1[i] = 0;
+            }
+            else {
+                if(start == -1) {
+                    arr1[i] = 100001;
+                }
+                else {
+                    arr1[i] = i - start;
+                }
+            }
+        }
+        start = -1;
+        for(int i = N - 1; i > -1; i--) {
+            if(S.charAt(i) == C) {
+                start = i;
+                arr2[i] = 0;
+            }
+            else {
+                if(start != -1) {
+                    arr2[i] = Math.min(arr1[i],start - i);
+                }
+                else {
+                    arr2[i] = arr1[i];
+                }
+            }
+        }
+        return arr2;
     }
 }
